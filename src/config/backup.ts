4de22f68de8b6d30e96dbdc9b4ff4ce4fd81b602
Ferdiaz Rifqi Prasida <ferdiazprasida@gmail.com>
2026-08-26 21:50:50 +0700

    Update backup.ts

--- src/config/backup.ts
+++ src/config/backup.ts
@@ -2,4 +2,4 @@ export const YKN_BACKUP_URL =
     'https://backup-tv.ykn.my.id';
 
 export const YKN_ANDROID_APK_DOWNLOAD_URL =
-    'https://sfile.co/dUkPJEDCh7e';
+    'https://sfile.co/2eJxqDfmELt';
